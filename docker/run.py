1eb6ad72009f11ab9dfcd8f4100860d21204333e
atrostan <atrostan@cs.ubc.ca>
2023-09-19 11:44:05 -0700

    print info to parsb execution add docker run python script

---
+++ docker/run.py
@@ -0,0 +1,6 @@
+import docker
+def main():
+    return 
+
+if __name__ == '__main__':
+    main()
--- main.cpp
+++ main.cpp
@@ -10,6 +10,22 @@
 #include <omp.h>
 #include <random>
 
+void print_sep() {
+  for (int i = 0; i < 74; ++i) {
+    std::cout << "-";
+  }
+  std::cout << "\n";
+}
+
+void info(uint32_t n_threads, uint32_t k, float p) {
+  print_sep();
+  fmt::print("Parallel SlashBurn Parameters:\n");
+  print_sep();
+  fmt::print("| Number of Threads: {:^14}| p: {:^14}| k: {:^14} |\n",
+             n_threads, p, k);
+  print_sep();
+}
+
 int main(int argc, char *argv[]) {
   CLApp cli(argc, argv, "slashburn");
   if (!cli.ParseArgs())
@@ -21,7 +37,11 @@ int main(int argc, char *argv[]) {
   Bitmap bmap(g.num_nodes());
   uint32_t num_nodes = g.num_nodes();
   uint num_threads = cli.num_threads();
+  uint32_t k = num_nodes * percent;
+  info(num_threads, k, percent);
   SlashBurn sb = SlashBurn(g, n_neighbour_rounds, percent, bmap, num_threads);
   sb.write_permutation(cli.out_filename());
+
+  fmt::print("{}ms to complete {} iterations.\n", sb.time, sb.n_iters);
   return 0;
 }
